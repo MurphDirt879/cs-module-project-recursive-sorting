b739decdbd6c96e2e9efa2217d3f9afb6d261356
MurphDirt879 <boxert879@hotmail.com>
2020-05-19 16:32:19 -0400

    MVP

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,6 +3,23 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
+    x = 0
+    y = 0
+
+    for i in range(0, elements):
+        if x >= len(arrA):
+            merged_arr[i] = arrB[y]
+            y += 1
+        elif y >= len(arrB):
+            merged_arr[i] = arrA[x]
+            x += 1
+        elif arrA[x] < arrB[y]:
+            merged_arr[i] = arrA[x]
+            x += 1
+        elif arrA[x] > arrB[y]:
+            merged_arr[i] = arrB[y]
+            y += 1
+
     # Your code here
 
 
@@ -12,7 +29,10 @@ def merge(arrA, arrB):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # Your code here
-
+    if len(arr) > 1:
+        left = merge_sort(arr[0:len(arr)//2])
+        right = merge_sort(arr[len(arr)//2:])
+        arr = merge(left, right)
 
     return arr
 
@@ -20,14 +40,42 @@ def merge_sort(arr):
 # implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    start2 = mid + 1; 
+  
+    if (arr[mid] <= arr[start2]): 
+        return; 
 
+    while (start <= mid and start2 <= end): 
+  
+        if (arr[start] <= arr[start2]): 
+            start += 1; 
+        else: 
+            value = arr[start2]; 
+            index = start2; 
+
+            while (index != start): 
+                arr[index] = arr[index - 1]; 
+                index -= 1; 
+              
+            arr[start] = value; 
+  
+            start += 1; 
+            mid += 1; 
+            start2 += 1; 
 
     return arr
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    if (l < r): 
+  
+        m = l + (r - l) // 2; 
+  
+        merge_sort_in_place(arr, l, m); 
+        merge_sort_in_place(arr, m + 1, r); 
+  
+        merge_in_place(arr, l, m, r); 
 
     return arr
 
